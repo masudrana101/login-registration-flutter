5287babbf8fbe989a5bb5c74193485db3bc5eafc
MD Masud Rana <masud.cse.sub@gmail.com>
2023-10-02 16:55:42 +0600

    stack

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -21,98 +21,532 @@ class HomeScreen extends StatelessWidget {
         ],
 
       ),
-      body: Column(children: [
+      body:
+      Column(
+        children: [
+          Stack(children: [
 
-        Align(
-          alignment: Alignment.center,
-          child: Container(
-            height: 100,
-            width: 200,
-            padding: EdgeInsets.all(20),
-            margin: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black, width: 4),
-              borderRadius: BorderRadius.circular(8),
-              boxShadow: [
-                new BoxShadow(
-                  color: Colors.orange,
-                  offset: new Offset(6.0, 6.0),
-                )
-              ],
+            Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                height: 100,
+                width: 100,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 4),
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.orange,
+                      offset: new Offset(6.0, 6.0),
+                    )
+                  ],
+                ),
+                child: Align(alignment: Alignment.center, child: Text("Hello", style: TextStyle(fontSize: 15),)),
+
+
+              ),
             ),
-            child: Align(alignment: Alignment.center, child: Text("Babgladesh", style: TextStyle(fontSize: 20),)),
 
-          ),
-        ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                height: 100,
+                width: 100,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 4),
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.orange,
+                      offset: new Offset(6.0, 6.0),
+                    )
+                  ],
+                ),
+                child: Align(alignment: Alignment.center, child: Text("Hello", style: TextStyle(fontSize: 15),)),
 
-        Align(
-          alignment: Alignment.topRight,
-          child: Container(
-            height: 100,
-            width: 200,
-            padding: EdgeInsets.all(20),
-            margin: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black, width: 4),
-              borderRadius: BorderRadius.circular(8),
-              boxShadow: [
-                new BoxShadow(
-                  color: Colors.orange,
-                  offset: new Offset(6.0, 6.0),
-                )
-              ],
+
+              ),
             ),
-            child: Align(alignment: Alignment.center, child: Text("Babgladesh", style: TextStyle(fontSize: 20),)),
 
-          ),
-          
-        ),
-        ElevatedButton(onPressed: () {}, child: Text('Save')),
 
-        Align(
-          alignment: Alignment.centerLeft,
-          child: Container(
-            height: 100,
-            width: 200,
-            padding: EdgeInsets.all(20),
-            margin: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black, width: 4),
-              borderRadius: BorderRadius.circular(8),
-              boxShadow: [
-                new BoxShadow(
-                  color: Colors.orange,
-                  offset: new Offset(6.0, 6.0),
-                )
-              ],
+
+            Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                height: 100,
+                width: 100,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 4),
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.orange,
+                      offset: new Offset(6.0, 6.0),
+                    )
+                  ],
+                ),
+                child: Align(alignment: Alignment.center, child: Text("Hello", style: TextStyle(fontSize: 15),)),
+
+
+              ),
             ),
-            child: Align(alignment: Alignment.center, child: Text("Babgladesh", style: TextStyle(fontSize: 20),)),
 
+
+          ],
           ),
-        ),
 
-        Align(
-          alignment: Alignment.bottomRight,
-          child: Container(
-            height: 100,
-            width: 200,
-            padding: EdgeInsets.all(20),
-            margin: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black, width: 4),
-              borderRadius: BorderRadius.circular(8),
-              boxShadow: [
-                new BoxShadow(
-                  color: Colors.orange,
-                  offset: new Offset(6.0, 6.0),
+          Stack(children: [
+            Align(
+                alignment: Alignment.bottomLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 35),
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Save 1')),
+
                 )
-              ],
             ),
-            child: Align(alignment: Alignment.center, child: Text("Babgladesh", style: TextStyle(fontSize: 20),)),
 
+            Align(
+              alignment: Alignment.bottomCenter,
+
+              child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Save 2')),
+
+
+            ),
+
+            Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 40),
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Save 3')),
+
+                )
+            ),
+
+          ],
           ),
-        ),
-      ],
+
+
+
+
+
+          Stack(children: [
+
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Container(
+                height: 100,
+                width: 100,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 4),
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.orange,
+                      offset: new Offset(6.0, 6.0),
+                    )
+                  ],
+                ),
+                child: Align(alignment: Alignment.center, child: Text("Hello 3", style: TextStyle(fontSize: 15),)),
+
+
+              ),
+            ),
+
+
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 100,
+                width: 100,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 4),
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.orange,
+                      offset: new Offset(6.0, 6.0),
+                    )
+                  ],
+                ),
+                child: Align(alignment: Alignment.center, child: Text("Hello", style: TextStyle(fontSize: 15),)),
+
+
+              ),
+
+            ),
+
+
+
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Container(
+                height: 100,
+                width: 100,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 4),
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.orange,
+                      offset: new Offset(6.0, 6.0),
+                    )
+                  ],
+                ),
+                child: Align(alignment: Alignment.center, child: Text("Hello 3", style: TextStyle(fontSize: 15),)),
+
+
+              ),
+            ),
+
+
+          ],
+          ),
+
+          Stack(children: [
+            Align(
+                alignment: Alignment.bottomLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 35),
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Save 1')),
+
+                )
+            ),
+
+            Align(
+              alignment: Alignment.bottomCenter,
+
+              child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Save 2')),
+
+
+            ),
+
+            Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 40),
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Save 3')),
+
+                )
+            ),
+
+          ],
+          ),
+
+          Stack(children: [
+
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Container(
+                height: 100,
+                width: 100,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 4),
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.orange,
+                      offset: new Offset(6.0, 6.0),
+                    )
+                  ],
+                ),
+                child: Align(alignment: Alignment.center, child: Text("Hello 3", style: TextStyle(fontSize: 15),)),
+
+
+              ),
+            ),
+
+
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 100,
+                width: 100,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 4),
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.orange,
+                      offset: new Offset(6.0, 6.0),
+                    )
+                  ],
+                ),
+                child: Align(alignment: Alignment.center, child: Text("Hello", style: TextStyle(fontSize: 15),)),
+
+
+              ),
+
+            ),
+
+
+
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Container(
+                height: 100,
+                width: 100,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 4),
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.orange,
+                      offset: new Offset(6.0, 6.0),
+                    )
+                  ],
+                ),
+                child: Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Hello 3",
+                      style: TextStyle(fontSize: 15),
+                    )),
+
+
+              ),
+            ),
+
+
+          ],
+          ),
+
+        Stack(children: [
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 35),
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Save 1')),
+
+                )
+            ),
+
+            Align(
+                alignment: Alignment.bottomCenter,
+
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Save 2')),
+
+
+            ),
+
+            Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 40),
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Save 3')),
+
+                )
+            ),
+
+          ],
+          ),
+
+
+
+          Stack(children: [
+
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Container(
+                height: 50,
+                width: 50,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 4),
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.orange,
+                      offset: new Offset(6.0, 6.0),
+                    )
+                  ],
+                ),
+                child: Align(alignment: Alignment.center, child: Text("He 3", style: TextStyle(fontSize: 5),)),
+
+
+              ),
+            ),
+
+
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 100),
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  padding: EdgeInsets.all(20),
+                  margin: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 4),
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: [
+                      new BoxShadow(
+                        color: Colors.orange,
+                        offset: new Offset(6.0, 6.0),
+                      )
+                    ],
+                  ),
+                  child: Align(alignment: Alignment.center, child: Text("He 4", style: TextStyle(fontSize: 10),)),
+
+
+                ),
+              ),
+
+            ),
+
+
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 100),
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  padding: EdgeInsets.all(20),
+                  margin: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 4),
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: [
+                      new BoxShadow(
+                        color: Colors.orange,
+                        offset: new Offset(6.0, 6.0),
+                      )
+                    ],
+                  ),
+                  child: Align(alignment: Alignment.center, child: Text("He 4", style: TextStyle(fontSize: 10),)),
+
+
+                ),
+              ),
+
+            ),
+
+
+
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Container(
+                height: 50,
+                width: 50,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 4),
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.orange,
+                      offset: new Offset(6.0, 6.0),
+                    )
+                  ],
+                ),
+                child: Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "He4",
+                      style: TextStyle(fontSize: 10),
+                    )),
+
+
+              ),
+            ),
+
+
+          ],
+          ),
+
+          Stack(children: [
+            Align(
+                alignment: Alignment.bottomLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Save 1')),
+
+                )
+            ),
+
+            Align(
+              alignment: Alignment.bottomCenter,
+
+              child: Padding(
+                padding: const EdgeInsets.only(right: 100),
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Save 2')),
+              ),
+
+
+            ),
+
+            Align(
+                alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 100),
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text('Save 3')),
+                  ),
+
+
+            ),
+            Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Save 4')),
+
+                )
+            ),
+
+          ],
+          ),
+
+
+
+
+        ],
       ),
 
 
